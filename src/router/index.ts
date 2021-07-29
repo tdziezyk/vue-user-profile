2a4585b8bab1c15288cacabc58791b354d57466c
Tomasz Dzieżyk <tomasz.dziezyk@gmail.com>
2021-07-29 22:15:33 +0200

    add redirecting to the edit page when page not founded

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,6 +22,12 @@ const routes: Array<RouteConfig> = [
     name: AppRoutes.ProfilePreview,
     component: UserProfilePreview,
   },
+  {
+    path: "*",
+    redirect: {
+      name: AppRoutes.EditProfile,
+    },
+  },
 ];
 
 const router = new VueRouter({
